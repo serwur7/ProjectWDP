6227565bd9ab78438d6fa35570162b80235ebac6
serwur7 <49037456+serwur7@users.noreply.github.com>
2020-06-29 00:48:31 +0200

    Add files via upload

---
+++ Talia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Talia
+{
+
+
+    
+
+    
+    string deck = new List<string>();
+    foreach (string symbol in Symbole)
+        {
+        foreach(string wartosc in Wartosci)
+            {
+            deck.Add(symbol+wartosc)
+            }
+        }
+Console.WriteLine(deck);
+}
+
